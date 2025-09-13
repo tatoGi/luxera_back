07a0d851be7677e04594d33cda37578d93dcea17
tatoGI <workspace.tato@gmail.com>
2025-09-13 22:09:22 +0400

    fix: add missing forward slash in Google OAuth redirect URL

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('APP_URL') . app()->getLocale() . '/auth/google/callback',
+        'redirect' => env('APP_URL') . '/' . app()->getLocale() . '/auth/google/callback',
     ],
 
 ];
