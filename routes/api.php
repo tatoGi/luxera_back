c4e7eee7ff0a70385127982dbbaeaf027bfa8071
tatoGI <workspace.tato@gmail.com>
2025-09-13 21:05:14 +0400

    chore: remove sensitive Google OAuth credentials from .env.example

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,3 @@ use App\Http\Controllers\Website\Auth\RegisterController as WebsiteRegisterContr
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
