8ccf8de6322ac73ef2d35af45c78957fc288dd81
tatoGI <workspace.tato@gmail.com>
2025-09-24 18:26:29 +0400

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,12 +8,22 @@ class Cors
 {   
     public function handle($request, Closure $next)
     {
+        $allowedOrigins = [
+            'https://luxeragift.netlify.app',
+            'http://localhost:3000', // for local dev
+        ];
+
+        $origin = $request->headers->get('Origin');
+
         $headers = [
-            'Access-Control-Allow-Origin'      => 'https://luxeragift.netlify.app',
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Headers'     => 'Content-Type, X-Auth-Token, Origin, Authorization, X-CSRF-TOKEN, X-XSRF-TOKEN',
             'Access-Control-Allow-Credentials' => 'true',
         ];
+
+        if (in_array($origin, $allowedOrigins)) {
+            $headers['Access-Control-Allow-Origin'] = $origin;
+        }
 
         if ($request->getMethod() === "OPTIONS") {
             return response()->json('OK', 200, $headers);
